2e6ae2b69d7d74498eaedf3e922402354c288ed0
Augusto Sergio Diaz Polo <adiazpolo17@correo.unicordoba.edu.co>
2022-07-04 19:59:03 -0500

    Primera versión ventana principal Fuentes: PT SANS

--- src/appdeingles/AppDeIngles.java
+++ src/appdeingles/AppDeIngles.java
@@ -15,7 +15,7 @@ public class AppDeIngles {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new VentanaPrincipal().setVisible(true);
     }
     
 }
